d3921058c9a7941ee6e5d18bf7a11261fbe67906
rxqvx <henriquekenji12@gmail.com>
2022-05-04 13:49:45 -0300

    fix: app global styles error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/configWebSite.css';
 
-//parte da admRoom e userRoom
-import '../styles/globals.css'
-import '../styles/Home.module.css'
-
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
