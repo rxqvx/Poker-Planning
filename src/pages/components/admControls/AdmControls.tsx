cff8368df69dae3d5251e870b5bfa6ff0de74702
rxqvx <henriquekenji12@gmail.com>
2022-06-27 11:28:21 -0300

    refac: refaczada clean folders and code

--- src/pages/components/admControls/AdmControls.tsx
+++ src/pages/components/admControls/AdmControls.tsx
@@ -6,25 +6,25 @@ interface AdmControlsProps {
   setShowVotes(show: boolean): void;
   setUsers(cb: any): void;
   setDisable(cb: any): void;
-} 
+}
 // function mostrar(socket, setShowVotes){
 //   setShowVotes(true)
 //   socket.emit('admin-show-vote')
 // }
 
-const AdmControls: React.FC<AdmControlsProps> = ({setShowVotes, setUsers, setDisable }) => {
-  function showUserCards(){
+const AdmControls: React.FC<AdmControlsProps> = ({ setShowVotes, setUsers, setDisable }) => {
+  function showUserCards() {
     setShowVotes(true)
 
     setDisable(true);
   }
-  
-  function resetUserCards(){
+
+  function resetUserCards() {
     setShowVotes(false)
     setDisable(false);
     // assim que o admin clickou no reset, passar todos os votevalues e isvoted como false de todos os users
     setUsers(oldUsers => oldUsers.map(u => {
-        return {...u, voteValue: '-', isVoted: false}
+      return { ...u, voteValue: '-', isVoted: false }
     }));
   }
 
@@ -35,14 +35,14 @@ const AdmControls: React.FC<AdmControlsProps> = ({setShowVotes, setUsers, setDis
           <p className={styles.titleAdminControl}>Admin Controls</p>
         </div>
         <div className={styles.buttonsFather}>
-        {/* onClick={() => setShowVotes(true)} */}
+          {/* onClick={() => setShowVotes(true)} */}
           <button className={styles.displayButtons} onClick={showUserCards}>Show Votes</button>
           <button className={styles.displayButtons} onClick={resetUserCards} >Reset Votes</button>
         </div>
-        </section>
+      </section>
     </>
   );
-  
+
 }
 
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-// Library's
-import React,{useState} from 'react';
+// Libraries
+import React, { useState } from 'react';
 import axios from 'axios';
 import { NextRouter, useRouter } from 'next/router';
 
@@ -11,112 +11,109 @@ import InputSection from './components/inputSection/InputSection'
 import FooterHome from './components/footerHome/FooterHome'
 import ContentHome from './components/contentHome/ContentHome'
 
-
-const HomePage = () : JSX.Element =>  {
+const HomePage = (): JSX.Element => {
   const [name, setName] = useState<string>('');
   const [nameError, setNameError] = useState<string>('');
-
-  const[room, setRoom] = useState<string>('');
-  const[roomError, setRoomError] = useState<string>('');
-
-  const router : NextRouter = useRouter();
+  const [room, setRoom] = useState<string>('');
+  const [roomError, setRoomError] = useState<string>('');
+  const router: NextRouter = useRouter();
   const { roomName } = router.query;
 
-  const handleNameChange = (e : React.ChangeEvent<HTMLInputElement>) : void => {
-      if(e.target.value) {
-          setNameError(undefined);
-      }
-      setName(e.target.value);
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    if (e.target.value) {
+      setNameError(undefined);
+    }
+    setName(e.target.value);
   }
-  const handleRoomChange = (e: React.ChangeEvent<HTMLInputElement>) : void => {
-    if(e.target.value) {
-        setRoomError(undefined);
+  const handleRoomChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    if (e.target.value) {
+      setRoomError(undefined);
     }
     setRoom(e.target.value);
   }
 
-  const submitNickAndRoom = async () : Promise<any> => {
-      if(!name || name.length < 2) {
-        setNameError('*Obrigatório e no Mínimo de 2 caracteres');
-        return;
-      }
-      if(!room || room.length < 2) {
-        setRoomError('*Obrigatório e no Mínimo de 2 caracteres');
-        return;
-      }
-      try {
-          const payload = { userName: name,  roomName: room };
-          const { data } = await axios.post('/api/entryRoomUser', payload);
-          sessionStorage.setItem('users',JSON.stringify(data.users));
-          sessionStorage.setItem('room',JSON.stringify(data.room));
-          sessionStorage.setItem('myUser',JSON.stringify(data.myUser));
-
-          window.open(`/UserRoom?roomName=${room}&nameUser=${name}`, "_self");
-      } catch(err) {alert("Error in the request")}
-  }
-
-  const submitOnlyNick = async () : Promise<any> => {
-    if(!name || name.length < 2) {
-        setNameError('*Obrigatório e no Mínimo de 2 caracteres');
-        return;
+  const submitNickAndRoom = async (): Promise<any> => {
+    if (!name || name.length < 2) {
+      setNameError('*Obrigatório e no Mínimo de 2 caracteres');
+      return;
+    }
+    if (!room || room.length < 2) {
+      setRoomError('*Obrigatório e no Mínimo de 2 caracteres');
+      return;
     }
     try {
-        const payload = { userName: name,  roomName: roomName};
-        const { data } = await axios.post('/api/entryRoomUser', payload);
-        sessionStorage.setItem('users',JSON.stringify(data.users));
-        sessionStorage.setItem('room',JSON.stringify(data.room));
-        window.open(`/UserRoom?roomName=${roomName}&nameUser=${name}`, "_self");
-    } catch(err) {alert("Error in the request")}
+      const payload = { userName: name, roomName: room };
+      const { data } = await axios.post('/api/entryRoomUser', payload);
+      sessionStorage.setItem('users', JSON.stringify(data.users));
+      sessionStorage.setItem('room', JSON.stringify(data.room));
+      sessionStorage.setItem('myUser', JSON.stringify(data.myUser));
+
+      window.open(`/UserRoom?roomName=${room}&nameUser=${name}`, "_self");
+    } catch (err) { alert("Error in the request") }
   }
 
-  if(!roomName){
+  const submitOnlyNick = async (): Promise<any> => {
+    if (!name || name.length < 2) {
+      setNameError('*Obrigatório e no Mínimo de 2 caracteres');
+      return;
+    }
+    try {
+      const payload = { userName: name, roomName: roomName };
+      const { data } = await axios.post('/api/entryRoomUser', payload);
+      sessionStorage.setItem('users', JSON.stringify(data.users));
+      sessionStorage.setItem('room', JSON.stringify(data.room));
+      window.open(`/UserRoom?roomName=${roomName}&nameUser=${name}`, "_self");
+    } catch (err) { alert("Error in the request") }
+  }
+
+  if (!roomName) {
     return (
       <>
-        <HeaderHome/>
+        <HeaderHome />
         <ContentHome>
-            <ArticleLetz/>
-            <InputSection>
-                <Input
-                    label="Nickname"
-                    value={name}
-                    onChange={handleNameChange}
-                    error={nameError}
-                    tip="Insira o apelido que será representado na sala."
-                    placeholder="Ex.: klougod"
-                />
-                <Input
-                    label="Sala"
-                    value={room}
-                    onChange={handleRoomChange}
-                    error={roomError}
-                    tip="Digite o nome da sala."
-                    placeholder="Ex.: Tech Review"
-                />
-            <button onClick={submitNickAndRoom}className="btn btn-primary">Confirmar</button>
-            </InputSection>
+          <ArticleLetz />
+          <InputSection>
+            <Input
+              label="Nickname"
+              value={name}
+              onChange={handleNameChange}
+              error={nameError}
+              tip="Insira o apelido que será representado na sala."
+              placeholder="Ex.: klougod"
+            />
+            <Input
+              label="Sala"
+              value={room}
+              onChange={handleRoomChange}
+              error={roomError}
+              tip="Digite o nome da sala."
+              placeholder="Ex.: Tech Review"
+            />
+            <button onClick={submitNickAndRoom} className="btn btn-primary">Confirmar</button>
+          </InputSection>
         </ContentHome>
-        <FooterHome/>
+        <FooterHome />
       </>
     );
   }
   return (
     <>
-    <HeaderHome/>
-    <ContentHome>
-        <ArticleLetz/>
+      <HeaderHome />
+      <ContentHome>
+        <ArticleLetz />
         <InputSection>
-            <Input
-                label="Nickname"
-                value={name}
-                onChange={handleNameChange}
-                error={nameError}
-                tip="Insira o apelido que será representado na sala."
-                placeholder="Ex.: klougod"
-            />
-            <button onClick={submitOnlyNick} className="btn btn-primary">Confirmar</button>
+          <Input
+            label="Nickname"
+            value={name}
+            onChange={handleNameChange}
+            error={nameError}
+            tip="Insira o apelido que será representado na sala."
+            placeholder="Ex.: klougod"
+          />
+          <button onClick={submitOnlyNick} className="btn btn-primary">Confirmar</button>
         </InputSection>
-    </ContentHome>
-    <FooterHome/>
+      </ContentHome>
+      <FooterHome />
     </>
   );
 }
